8483920c75338ac069498ab44e039e3b34ec5e59
Joseph Dalrymple <me@swivel.in>
2014-07-13 20:55:42 -0500

    Initial development commit

---
+++ content-item.js
@@ -0,0 +1,31 @@
+var ViewContentItem = (function(){
+	var Constructor = function(node, value, handler){
+		this._node = node;
+
+		this.set(value), this._handler = handler;
+	};
+
+	Constructor.prototype = {
+		_node: null,
+		_value: null,
+		_handler: null,
+
+		set: function(val){
+			this._value = val;
+
+			this._node.nodeValue = val;
+
+			if(this._handler instanceof Function) this._handler.call(this);
+
+			return this.get();
+		},
+		get: function(){
+			return this.toString();
+		},
+		toString: function(){
+			return this._value;
+		}
+	}
+
+	return Constructor;
+})();
